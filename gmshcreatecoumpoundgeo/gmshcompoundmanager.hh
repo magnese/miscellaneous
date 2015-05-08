f6c05700bbc37713e2f3d5c9ec060a4b23482862
Marco Agnese <m.agnese13@imperial.ac.uk>
2015-05-08 19:37:54 +0100

    created project

---
+++ gmshcreatecoumpoundgeo/gmshcompoundmanager.hh
@@ -0,0 +1,131 @@
+#ifndef GMSHCOMPOUNDMANAGER_HH
+#define GMSHCOMPOUNDMANAGER_HH
+
+#include <string>
+#include <array>
+
+#include "GModel.h"
+
+class GMSHCompoundManager
+{
+  public:
+  GMSHCompoundManager(const std::string& domainFileName,const std::string& interfaceFileName,const std::string& holeFileName):
+    gmodelptrs_(),hashole_(false)
+  {
+    // load domain
+    domain()=new GModel();
+    domain()->setFactory("Gmsh");
+    domain()->readGEO(domainFileName);
+    // load interface
+    interface()=new GModel();
+    interface()->setFactory("Gmsh");
+    interface()->readMSH(interfaceFileName);
+    // load hole (if present)
+    hole()=new GModel();
+    hole()->setFactory("Gmsh");
+    if(holeFileName!="")
+    {
+      hole()->readGEO(holeFileName);
+      hashole_=true;
+    }
+    // create compound
+    createCompound();
+  }
+
+  ~GMSHCompoundManager()
+  {
+    for(auto& model:gmodelptrs_)
+      delete model;
+  }
+
+  inline GModel*& domain()
+  {
+    return gmodelptrs_[0];
+  }
+  inline GModel*& interface()
+  {
+    return gmodelptrs_[1];
+  }
+  inline GModel*& hole()
+  {
+    return gmodelptrs_[2];
+  }
+  inline GModel*& compound()
+  {
+    return gmodelptrs_[3];
+  }
+
+  inline bool& hasHole()
+  {
+    return hashole_;
+  }
+
+  void createCompound()
+  {
+    if(compound()!=nullptr)
+      delete gmodelptrs_[3];
+    compound()=new GModel();
+    compound()->setFactory("Gmsh");
+
+    // add domain to compound gmodel
+    std::vector<GEdge*> domainEdges(0);
+    addGModelToCompound(*domain(),domainEdges);
+
+    // add interface to compound gmodel
+    addMeshToCompound();
+
+    // add hole to compound gmodel (if present)
+    std::vector<GEdge*> holeEdges(0);
+    if(hashole_)
+      addGModelToCompound(*hole(),holeEdges);
+
+    // add line loops and faces to compound gmodel
+    std::vector<std::vector<GEdge*> > lineLoop(1,domainEdges);
+    compound()->addPlanarFace(lineLoop);
+  }
+
+  private:
+  std::array<GModel*,4> gmodelptrs_;
+  bool hashole_;
+
+  void addGModelToCompound(GModel& model,std::vector<GEdge*>& edges)
+  {
+    unsigned int vtxInsertionCounter(0);
+    std::vector<GVertex*> vtxInsertedPtr(0);
+    std::vector<GVertex*> vtxPtr(2,nullptr);
+    std::vector<int> vtxInsertionMap(model.getNumVertices()+1,-1);
+    for(typename GModel::eiter it=model.firstEdge();it!=model.lastEdge();++it)
+    {
+      // get edge physical ID
+      unsigned int physicalID(((*it)->getPhysicalEntities())[0]);
+      // add first vertex
+      vtxPtr[0]=(*it)->getBeginVertex();
+      if(vtxInsertionMap[vtxPtr[0]->tag()]==-1)
+      {
+        vtxInsertedPtr.push_back(compound()->addVertex(vtxPtr[0]->x(),vtxPtr[0]->y(),vtxPtr[0]->z(),
+                                                        vtxPtr[0]->prescribedMeshSizeAtVertex()));
+        vtxInsertionMap[vtxPtr[0]->tag()]=vtxInsertionCounter;
+        ++vtxInsertionCounter;
+      }
+      vtxPtr[0]=vtxInsertedPtr[vtxInsertionMap[vtxPtr[0]->tag()]];
+      // add last vertex
+      vtxPtr[1]=(*it)->getEndVertex();
+      if(vtxInsertionMap[vtxPtr[1]->tag()]==-1)
+      {
+        vtxInsertedPtr.push_back(compound()->addVertex(vtxPtr[1]->x(),vtxPtr[1]->y(),vtxPtr[1]->z(),
+                                 vtxPtr[1]->prescribedMeshSizeAtVertex()));
+        vtxInsertionMap[vtxPtr[1]->tag()]=vtxInsertionCounter;
+        ++vtxInsertionCounter;
+      }
+      vtxPtr[1]=vtxInsertedPtr[vtxInsertionMap[vtxPtr[1]->tag()]];
+      // add edge
+      edges.push_back(compound()->addLine(vtxPtr[0],vtxPtr[1]));
+      (*edges.rbegin())->addPhysicalEntity(physicalID);
+    }
+  }
+
+  void addMeshToCompound()
+  {}
+};
+
+#endif //GMSHCOMPOUNDMANAGER_HH
---
+++ gmshcreatecoumpoundgeo/gmshcreatecompoundgeo.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "Gmsh.h"
+#include "GModel.h"
+#include "MTetrahedron.h"
+
+#include "gmshcompoundmanager.hh"
+
+#define DEBUG_STATUS 1
+
+enum GmshAlgorithmType {automatic=2,delaunay=5,frontal=6,meshadapt=1};
+
+int main(int argc,char** argv)
+{
+  // init gmsh
+  GmshInitialize(argc, argv);
+  GmshSetOption("General","Terminal",1.);
+  #if DEBUG_STATUS
+  GmshSetOption("General","Verbosity",99.);
+  #endif
+  GmshSetOption("Mesh","Algorithm",static_cast<double>(automatic));
+
+  // get geometries file names
+  std::string domainFileName;
+  std::string interfaceFileName;
+  std::string holeFileName;
+  if(argc<3)
+  {
+    std::cout<<"Program usage: "<<std::endl;
+    std::cout<<"./gmshreader domain.geo interface.geo [hole.geo]"<<std::endl;
+    return 1;
+  }
+  else
+  {
+    domainFileName=argv[1];
+    std::cout<<"Domain: "<<domainFileName<<std::endl;
+    interfaceFileName=argv[2];
+    std::cout<<"Interface: "<<interfaceFileName<<std::endl;
+    if(argc>3)
+    {
+      holeFileName=argv[3];
+      std::cout<<"Hole: "<<holeFileName<<std::endl;
+    }
+  }
+
+  // create gmodels
+  GMSHCompoundManager compoundManager(domainFileName,interfaceFileName,holeFileName);
+
+  // dump compound geo file
+  std::string compoundFileName("compound.geo");
+  compoundManager.compound()->writeGEO(compoundFileName,true,false);
+
+  // finalize gmsh and return
+  GmshFinalize();
+  return 0;
+}
