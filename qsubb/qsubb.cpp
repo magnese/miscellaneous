6c698c55dec0c18ce91f9ac6f0cc591b3357bbf7
Marco Agnese <m.agnese13@imperial.ac.uk>
2015-07-23 14:55:31 +0100

    major semplification

--- qsubb/qsubb.cpp
+++ qsubb/qsubb.cpp
@@ -6,55 +6,47 @@
 int main(int argc,char** argv)
 {
 
-  std::string user(getenv("USER"));
-  std::string pwd(getenv("PWD"));
+  const std::string user(getenv("USER"));
+  const std::string pwd(getenv("PWD"));
 
   if(argc<3)
   {
-    std::cout<<"qsubb v0.2"<<std::endl;
+    std::cout<<"qsubb v0.3"<<std::endl;
     std::cout<<"Description:\t qsubb creates a script and submit the job to qsub."<<std::endl;
     std::cout<<"Usage:\t\t qsubb program_name output_name [node_number](01 default)"<<std::endl;
     std::cout<<"\t\t Move to the folder where the program is saved."<<std::endl;
     std::cout<<"\t\t The output will be saved in /scratchcompXX/"<<user<<"/output_name/ where XX is the node_number."<<std::endl;
     std::cout<<"Author:\t\t Marco Agnese (m.agnese13@imperial.ac.uk)."<<std::endl;
+    return 0;
   }
-  else
+
+  const std::string program_name(argv[1]);
+  const std::string output_name(argv[2]);
+  const std::string output_node(argc>3?argv[3]:"01");
+
+  std::ofstream file(program_name+"_qsub.sh");
+  if(!file.is_open())
   {
-    std::string program_name(argv[1]);
-    std::string output_name(argv[2]);
-    std::string output_node("01");
-
-    if(argc>3)
-      output_node=argv[3];
-
-    const std::string qsub_script_name(program_name+"_qsub.sh");
-    std::ofstream qsub_script(qsub_script_name);
-    if(qsub_script.is_open())
-    {
-      qsub_script<<"#! /bin/bash"<<std::endl;
-      qsub_script<<"#PBS -N "<<program_name<<std::endl;
-      qsub_script<<"#PBS -m abe"<<std::endl;
-      qsub_script<<"#PBS -q ubuntu"<<std::endl;
-      qsub_script<<"#PBS -o /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<"/"<<output_name<<".out"<<std::endl;
-      qsub_script<<"#PBS -e /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<"/"<<output_name<<".err"<<std::endl;
-      qsub_script<<"mkdir -p /scratchcomp"<<output_node<<"/"<<user<<std::endl;
-      qsub_script<<"mkdir -p /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<std::endl;
-      qsub_script<<"cd /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<std::endl;
-      qsub_script<<pwd<<"/"<<program_name<<std::endl;
-      qsub_script.close();
-    }
-    else
-    {
-      std::cout<<"ERROR: Impossible to create the file "<<qsub_script_name<<" ."<<std::endl;
-      return 1;
-    }
-
-    std::string command_chmod="chmod 755 "+qsub_script_name;
-    system(command_chmod.c_str());
-
-    std::string command_qsub="qsub "+qsub_script_name;
-    system(command_qsub.c_str());
+    std::cout<<"ERROR: Impossible to create the file "<<program_name<<"_qsub.sh ."<<std::endl;
+    return 1;
   }
+
+  file<<"#! /bin/bash"<<std::endl;
+  file<<"#PBS -N "<<program_name<<std::endl;
+  file<<"#PBS -m abe"<<std::endl;
+  file<<"#PBS -q standard"<<std::endl;
+  file<<"#PBS -o /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<"/"<<output_name<<".out"<<std::endl;
+  file<<"#PBS -e /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<"/"<<output_name<<".err"<<std::endl;
+  file<<"mkdir -p /scratchcomp"<<output_node<<"/"<<user<<std::endl;
+  file<<"mkdir -p /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<std::endl;
+  file<<"cd /scratchcomp"<<output_node<<"/"<<user<<"/"<<output_name<<std::endl;
+  file<<pwd<<"/"<<program_name<<std::endl;
+
+  const std::string command_chmod("chmod 755 "+program_name+"_qsub.sh");
+  system(command_chmod.c_str());
+
+  const std::string command_qsub("qsub "+program_name+"_qsub.sh");
+  system(command_qsub.c_str());
 
   return 0;
 }
